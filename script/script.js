1032b3f4bfa860ab24c0ce8412d13904c8230194
Daniel Oliveira <danielcoliv98@gmail.com>
2022-02-15 13:27:18 -0300

    Recebe quiz do servidor

--- script/script.js
+++ script/script.js
@@ -1,1 +1,25 @@
-const allQuiz = document.querySelector(".all-quiz")
+// API DO QUIZ
+const QUIZ_API = "https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes"
+
+// LISTA DOS QUIZ DO SERVIDOR
+const allQuiz = document.querySelector(".all-quiz .quiz-container")
+
+
+function searchQuiz() {
+    const promise = axios.get(QUIZ_API)
+    promise.then(quizDisplay);
+
+}
+
+function quizDisplay(response) {
+
+    response.data.forEach(element => {
+        allQuiz.innerHTML += `
+        <li class="quiz" id="${element.id}">
+            <img src="${element.image}" alt="">
+            <h3>${element.title}</h3>
+        </li>
+        `
+    });
+}
+searchQuiz()
